ac88276d337221d7bb2736dfb7b065ece69e5f26
Shane Holloway <shane.holloway@ieee.org>
2016-12-28 10:14:52 -0700

    Added stage-3 support

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ module.exports = exports = function preset(context, opts={}) {
     opts.targets = {node: 'current'}
 
   let presets =
-    [ [require('babel-preset-env'), opts] ]
+    [ [require('babel-preset-env'), opts]
+    , require('babel-preset-stage-3')]
   let plugins = 
     [ require('babel-plugin-offside-js')
     , require('babel-plugin-transform-class-properties')
