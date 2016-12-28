4f8971d5ca55ecdd340413a635d24e9e2fd1d7cc
Shane Holloway <shane.holloway@ieee.org>
2016-12-28 12:30:27 -0700

    Removed undeclared variables check, as it was complaining about 'require', 'module', and 'exports'

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ module.exports = exports = function preset(context, opts={}) {
 
   let presets =
     [ [require('babel-preset-env'), opts]
-    , require('babel-preset-stage-3')]
+    , require('babel-preset-stage-3')
+    ]
   let plugins = 
     [ require('babel-plugin-offside-js')
     , require('babel-plugin-transform-class-properties')
-    , require('babel-plugin-undeclared-variables-check') ]
+    ]
 
   return {presets, plugins} }
 
