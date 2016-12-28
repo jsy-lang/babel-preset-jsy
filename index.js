c85d496b140a31e532a599e8a1caf240bac30950
Shane Holloway <shane.holloway@ieee.org>
2016-12-28 13:05:46 -0700

    Added a default interpretation for browser:true

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 module.exports = exports = function preset(context, opts={}) {
   if (!opts.targets)
     opts.targets = {node: 'current'}
+  if (true === opts.targets.browser)
+    opts.targets.browser = 'last 1 versions, > 2% in US'
 
   let presets =
     [ [require('babel-preset-env'), opts]
