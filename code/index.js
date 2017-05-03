17d2137f6239c6ffc5008a0860661cdda8b66137
Shane Holloway <shane.holloway@ieee.org>
2017-05-03 17:46:40 -0600

    Added option for excluding stage_3 preset

--- code/index.js
+++ code/index.js
@@ -4,22 +4,24 @@ const bpi_offside_js = require('babel-plugin-offside-js')
 const bpi_class_props = require('babel-plugin-transform-class-properties')
 
 module.exports = exports = function preset(context, opts={}) ::
+  const opts_stage_3 = ! opts.no_stage_3
   const opts_offside = opts.offside
   delete opts.offside
 
-  if (!opts.targets) ::
+  if !opts.targets ::
     opts.targets = @{} node: 'current'
 
-  if (true === opts.targets.browser) ::
+  if true === opts.targets.browser ::
     opts.targets.browser = 'last 1 versions, > 2% in US'
 
-  let presets =
-    @[] @[] bps_env, opts
-      , @[] bps_stage_3
+  let presets = [], plugins = []
 
-  let plugins = 
-    @[] bpi_class_props
-      , @[] bpi_offside_js, opts_offside
+  presets.push @ [ bps_env, opts ]
+  if opts_stage_3 ::
+    presets.push @ [ bps_stage_3 ]
+
+  plugins.push @ [ bpi_class_props ]
+  plugins.push @ [ bpi_offside_js, opts_offside ]
 
   return @{} presets, plugins
 
