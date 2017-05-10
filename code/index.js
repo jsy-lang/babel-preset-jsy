4e67088a9fb7e02c20fdb30e55215c73abb44a9a
Shane Holloway <shane.holloway@ieee.org>
2017-05-10 13:24:55 -0600

    Added a lean preset flavor, and worked around a bug with current version of babel-preset-env

--- code/index.js
+++ code/index.js
@@ -2,15 +2,22 @@ const bps_env = require('babel-preset-env').default
 const bps_stage_3 = require('babel-preset-stage-3')
 const bpi_offside_js = require('babel-plugin-offside-js')
 const bpi_class_props = require('babel-plugin-transform-class-properties')
+const bpi_async_gen_fns = require('babel-plugin-transform-async-generator-functions')
 
 module.exports = exports = function preset(context, opts={}) ::
-  const opts_stage_3 = ! opts.no_stage_3 || opts.stage_3
+  const opts_stage_3 = 'stage_3' in opts ? opts.stage_3 : !opts.no_stage_3
   delete opts.stage_3; delete opts.no_stage_3; 
   const opts_offside = opts.offside
   delete opts.offside
 
   if !opts.targets ::
     opts.targets = @{} node: 'current'
+
+  if 'current' == opts.targets.node ::
+    const major_ver = /^v(\d+)\.\d\d/.exec @ process.version
+    if major_ver ::
+      // babel-preset-env has a bug handling two digit Node minor versions.
+      opts.targets.node = parseInt(major_ver[1]) + 0.9
 
   if true === opts.targets.browser ::
     opts.targets.browser = 'last 1 versions, > 2% in US'
@@ -20,6 +27,9 @@ module.exports = exports = function preset(context, opts={}) ::
   presets.push @ [ bps_env, opts ]
   if opts_stage_3 ::
     presets.push @ [ bps_stage_3 ]
+  else ::
+    // However, generator async * functions should still be supported
+    plugins.push @ [ bpi_async_gen_fns ]
 
   plugins.push @ [ bpi_class_props ]
   plugins.push @ [ bpi_offside_js, opts_offside ]
---
+++ code/lean.js
@@ -0,0 +1,4 @@
+const _basePreset_ = require('./index')
+
+module.exports = exports = function preset(context) ::
+  return _basePreset_ @ context, @{} stage_3: false
